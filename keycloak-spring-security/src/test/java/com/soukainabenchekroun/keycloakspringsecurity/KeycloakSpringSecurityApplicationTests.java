c2ef46b99057b3f7b3a09c90a47d384e3484b19f
soukainabenchekroun <soukainabenchekroun3@gmail.com>
2021-11-07 15:25:16 +0100

    init spring boot project

---
+++ keycloak-spring-security/src/test/java/com/soukainabenchekroun/keycloakspringsecurity/KeycloakSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soukainabenchekroun.keycloakspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
